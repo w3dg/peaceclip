d76b873535b59f8f102b0b0dc7bc2f2f7c21a953
DG <debopamgupta3000@gmail.com>
2020-06-05 13:09:24 +0530

    :sparkles: Add Visual changes

--- renderer.js
+++ renderer.js
@@ -5,10 +5,19 @@ const Datastore = require("nedb"),
 const app = new Vue({
   el: "#app",
   data: {
-    title: "😎 Peace Clip",
-    history: [],
+    title: "📎 Peace Clip",
+    clipHistory: [],
   },
   mounted() {
+    db.find({}, function (err, docs) {
+      const { data } = docs[docs.length - 1];
+      console.log(data);
+      for (let i = 0; i < data.length; i++) {
+        console.log("WAT", data[i]);
+        // this.clipHhistory.push(data[i]);
+        console.log(i);
+      }
+    });
     // setInterval(this.insertHistorytoDB, 1800000 );
     setInterval(this.insertHistorytoDB, 10000);
     setInterval(this.checkClipboard, 1000);
@@ -16,13 +25,13 @@ const app = new Vue({
   methods: {
     checkClipboard() {
       const text = clipboard.readText();
-      if (this.history[this.history.length - 1] !== text) {
-        this.history.push(text);
+      if (this.clipHistory[this.clipHistory.length - 1] !== text) {
+        this.clipHistory.push(text);
       }
     },
 
     insertHistorytoDB() {
-      db.insert({ data: this.history }, function (err, newDocs) {});
+      db.insert({ data: this.clipHistory }, function (err, newDocs) {});
     },
   },
 });
